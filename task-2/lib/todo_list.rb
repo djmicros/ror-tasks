c4e74245033cbecc29c5dd51ab78e359d0d2597d
Kuciel Adrian <10_kuciel@wierzba.wzks.uj.edu.pl>
2013-03-27 21:12:27 +0100

    All tests from task-2 GREEN

--- task-2/lib/todo_list.rb
+++ task-2/lib/todo_list.rb
@@ -1,6 +1,43 @@
 class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
-  def initialize(items=[])
+  def initialize(parameters=[])
+  @parameters = parameters
+  raise IllegalArgument if parameters[:db].nil?
   end
+  
+  def empty?
+  @parameters[:db].items_count == 0
+  end
+  
+  def size
+  @parameters[:db].items_count
+  end
+  
+  def << (item)
+  if item[:title] == ""
+  false
+  else
+  @parameters[:db].add_todo_item(item)
+  end
+  end
+  
+  def first
+  @parameters[:db].get_todo_item(0)
+  end
+  
+  def last
+  size = @parameters[:db].items_count
+  number = size-1
+  @parameters[:db].get_todo_item(number)
+  end
+  
+  def toggle_state (index)
+  if @parameters[:db].todo_item_completed?(index) == false
+  @parameters[:db].complete_todo_item(index,true)
+  else
+  @parameters[:db].complete_todo_item(index,false)
+  end
+  end
+  
 end
