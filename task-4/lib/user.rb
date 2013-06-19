9ed6a24eb0f374e5a61e0568b2dcc9fc3474d4a3
Kuciel Adrian <10_kuciel@wierzba.wzks.uj.edu.pl>
2013-06-19 19:59:43 +0200

    Broken task-4

--- task-4/lib/user.rb
+++ task-4/lib/user.rb
@@ -18,6 +18,39 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   
   
+  def find_user_by_surname(surname) 
+    found = User.where(:surname=> surname)
+	return found[0]
+  end
+  
+  def find_user_by_prefix(prefix) 
+    found = User.where('surname LIKE ?', "%#{prefix}%")
+	return found[0]
+  end
+  
+  def find_user_by_email(email) 
+    found = User.where(:email=> email)
+	return found[0]
+  end
+  
+  def authenticate_user(email, password) 
+    user = User.find_by_email(email)
+	return user.authenticate(password)
+  end
+  
+  def register_failed_login(index)
+    user = User.find(index)
+	current_count = user.failed_login_count
+    user.update_attribute(:failed_login_count, current_count + 1)
+  end
+  
+  def find_suspicious_users
+	users = User.where('failed_login_count > 2')
+  end
+  
+  def group_by_failed_logins
+    users = User.order("failed_login_count DESC")
+  end
  
   
 
